7a8161af879cc4ab03f1ff70c829418d7757662e
Mario <mario.rodriguez.izcoa@takeaway.com>
2015-12-17 16:42:37 +0100

    wrapping all in an endpoint

--- command.js
+++ command.js
@@ -2,13 +2,19 @@
 
 const request = require('request-promise');
 const bluebird = require('bluebird');
-const find = require('lodash.find');
+const find = require('lodash').find;
 
 const mappings = require('./mappings');
 const receivers = mappings.receivers;
 
+const protocol = 'http';
+const base = 'api';
+const token = '117eb81b3935ab6f19a21b23382b78f7';
+
 const serviceDiscovery = 'https://www.meethue.com/api/nupnp';
 let activeReceivers;
+let api ; 
+let url ; 
 
 function discover() {
     return request(serviceDiscovery).then((body) => {
@@ -21,19 +27,26 @@ function discover() {
         activeReceivers = withIps.reduce(
             (memo, r) => Object.assign(memo, {[r.name]: r.internalipaddress})
         );
+
+        const host = activeReceivers.internalipaddress  ; 
+        api =  `${protocol}://${host}/${base}/${token}`;
+
     });
 }
 
-function command(method, resource, payload, explicitDelay) {
+function command(method, endpoint, payload, explicitDelay) {
     var delay = explicitDelay || payload.transitiontime || 0;
 
     if (!activeReceivers) {
         return discover().then(() => command(...arguments));
     }
+    url = `${api}${endpoint}` ;
+
+    console.log ('Calling API:' + api ); 
 
     return request({
       method: method,
-      uri: resource,
+      uri: url,
       body: payload,
       json: true
     })
--- index.js
+++ index.js
@@ -6,14 +6,6 @@ const command = require('./command');
 const mappings = require('./mappings');
 const moods = mappings.moods;
 
-const receivers = mappings.receivers;
-
-const protocol = 'http';
-const host = '10.10.13.33';
-const base = 'api';
-const token = '117eb81b3935ab6f19a21b23382b78f7';
-const api = `${protocol}://${host}/${base}/${token}`;
-
 const port = 1337;
 const server = restify.createServer();
 
@@ -23,19 +15,19 @@ server.get('/:receiver/:bulb/:mood', function (req, res) {
     const receiver = req.params.receiver;
     const bulb = req.params.bulb;
     const mood = req.params.mood;
+    const endpoint = '/lights/1/state';
+
+	const heartbeat = Object.assign({on: true, transitiontime: 5}, moods.alive.color);
+	const fadeOut = Object.assign({}, heartbeat, {bri: 1, transitiontime: 5});
+	const off = {on: false};
+
+	command('PUT', endpoint, heartbeat)
+	    .then(() => command('PUT', endpoint, fadeOut))
+	    .then(() => command('PUT', endpoint, off))
+	    .then(() => console.log('Everything fine'));
 });
 
-const endpoint = '/lights/1/state';
-const url = api + endpoint;
 
-console.log('Using API URL:', url);
 
-const heartbeat = Object.assign({on: true, transitiontime: 5}, moods.alive.color);
-const fadeOut = Object.assign({}, heartbeat, {bri: 1, transitiontime: 5});
-const off = {on: false};
 
-command('PUT', url, heartbeat)
-    .then(() => command('PUT', url, fadeOut))
-    .then(() => command('PUT', url, off))
-    .then(() => console.log('Everything fine'));
 
