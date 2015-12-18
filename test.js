f18588ff269bf562e62a455c0d9e28ecb6b13687
aakrem <akrem.abayed@gmail.com>
2015-12-18 07:52:28 +0100

    test improved with commander

--- test.js
+++ test.js
@@ -1,7 +1,17 @@
 const request = require('request-promise');
+const program = require('commander');
 
-var host = 'http://localhost:1337/phpWoker/critical';
+var host = 'http://localhost:1337';
 
-request(host).then(() => {
-	console.log('yoo');
-});
+program
+  .option('-m, --mood <mood>', 'mood name')
+  .option('-b, --bulb <bulb>', 'bulb name')
+  .parse(process.argv);
+
+if (program.mood && program.bulb) {
+	host = `${host}/${program.bulb}/${program.mood}`;
+	console.log(host);
+	request(host).then(() => {
+		console.log('yoo');
+	});
+}
